1315811c35176420f5b465e6368b21f2e5e4ddc3
narendar-dev <your-email@example.com>
2026-08-28 18:35:30 +0530

    1st  project push Spring_Rest_POC CodeBase

---
+++ SpringRest-01-SimplePOC/src/main/java/in/nkn/SpringRest01SimplePocApplication.java
@@ -0,0 +1,13 @@
+package in.nkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest01SimplePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest01SimplePocApplication.class, args);
+	}
+
+}
