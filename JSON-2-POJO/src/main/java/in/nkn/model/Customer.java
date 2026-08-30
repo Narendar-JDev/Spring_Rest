d14e9e3e6928f1fe25a2d4864d97264472a3163e
narendar-dev <your-email@example.com>
2026-08-30 10:30:30 +0530

    JSON-2-POJO push Spring_Rest codebase

---
+++ JSON-2-POJO/src/main/java/in/nkn/model/Customer.java
@@ -0,0 +1,78 @@
+package in.nkn.model;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Customer {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Boolean active;
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String[] getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String[] languages) {
+		this.languages = languages;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", active=" + active
+				+ ", address=" + address + ", languages=" + Arrays.toString(languages) + "]";
+	}
+
+	// HAS-A property
+	private Address address;
+
+	// Array property
+	private String[] languages;
+}
